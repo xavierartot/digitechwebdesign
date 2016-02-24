910e2fe28a48c073257e92d592c4e92221589a7a
xavier <artotal@gmail.com>
2016-02-24 16:10:29 -0600

    logo done

--- lib/extras.php
+++ lib/extras.php
@@ -31,3 +31,12 @@ function excerpt_more() {
   return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'sage') . '</a>';
 }
 add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
+
+
+//function load_fonts() {
+    ////wp_register_style('googleFonts', '//fonts.googleapis.com/css?family=Roboto:400,300,200,400italic,500,700,100italic|Lato');
+  ////<link href='https://fonts.googleapis.com/css?family=Lato:400,700,300,400italic' rel='stylesheet' type='text/css'>
+    //wp_register_style('googleFonts', '//fonts.googleapis.com/css?family=Lato:400,700,300,400italic,700italic,900|Lato');
+    //wp_enqueue_style( 'googleFonts');
+  //}
+//add_action('wp_print_styles', __NAMESPACE__ . '\\load_fonts');
--- templates/header.php
+++ templates/header.php
@@ -1,23 +1,26 @@
+<div class='hero'>
+  <header class="banner navbar navbar-default navbar-static-top " role="banner">
+    <div class="">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only"><?= __('Toggle navigation', 'sage'); ?></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?>
+          <img src="<?php echo get_template_directory_uri(); ?>/dist/images/logo.png" alt="logo"/>
+        </a>
+      </div>
 
 
-<header class="banner navbar navbar-default navbar-static-top" role="banner">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only"><?= __('Toggle navigation', 'sage'); ?></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    </div>
-
-    <nav class="collapse navbar-collapse" role="navigation">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
+      <nav class="collapse navbar-collapse pull-right" role="navigation">
+        <?php
+        if (has_nav_menu('primary_navigation')) :
         wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'nav navbar-nav']);
-      endif;
-      ?>
-    </nav>
-  </div>
-</header>
+        endif;
+        ?>
+      </nav>
+    </div>
+  </header>
+</div>
